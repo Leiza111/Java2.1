ad8dd937fd439c621abee622cfada4036946b50f
Leiza Girfatova <leiza-rad@yandex.ru>
2021-10-07 21:20:15 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiketPraice = 115;
+        int bonusMile = 20;
+        int milesАccrued = tiketPraice / bonusMile;
+        System.out.println(milesАccrued);
+    }
+}

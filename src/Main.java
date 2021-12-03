2786dae067955a99fe57b770a8a4b6bda6ab4656
Leiza111 <leiza-rad@yandx.ru>
2021-12-03 13:21:56 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        int tiketPraice = 300000;
+        int tiketPraice = 620000;
         int bonusMile = 20;
         int milesАccrued = tiketPraice / bonusMile;
         System.out.println(milesАccrued);
